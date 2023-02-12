eba803a3b60849a95c0027ab27c06bdf21711344
Victoria <veejo90@gmail.com>
2023-02-12 00:14:45 -0500

    content added

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,95 @@
-
-import './App.css'
-
+import "./App.css";
 
 export default function App() {
-
-
   return (
-    <div className='wrapper'>
-    <Header/>
-    <Stuff/>
-    <Footer/>
+    <div className="wrapper">
+      <Header />
+      <Stuff />
+      <Footer />
     </div>
-  )
+  );
 }
 
 function Header() {
   return (
-    <div className='page-header'>
-    <h1> I am the Header</h1>
-    </div> 
-
-  )  
+    <div className="page-header">
+      <h1> Example Webpage</h1>
+    </div>
+  );
 }
 
 function Stuff() {
   return (
-  
-    <article className='page-body'>
-    <div className='container'>
-    <section>
-        <h2>Section 1</h2>
-        <p>bleh blah blew pop pop bing bap</p>
-      </section>
-      <section>
-        <h2>Section 2</h2>
-        <p>bleh blah blew pop pop bing bap</p>
-      </section>
-      <section>
-        <h2>Section 3</h2>
-        <p>bleh blah blew pop pop bing bap</p>
-      </section>
-
-    </div>
-    
-
+    <article className="page-body">
+      <div className="container">
+        <section>
+          <h2>Section 1</h2>
+          <p>
+            Phasellus pharetra dolor ut dolor porta tristique. Sed rhoncus
+            sagittis nisi, sed cursus diam. Aliquam orci elit, fringilla in
+            lobortis nec, blandit non leo. Phasellus pharetra dolor ut dolor
+            porta tristique. Sed rhoncus sagittis nisi, sed cursus diam. Aliquam
+            orci elit, fringilla in lobortis nec, blandit non leo. Phasellus
+            pharetra dolor ut dolor porta tristique. Sed rhoncus sagittis nisi,
+            sed cursus diam. Aliquam orci elit, fringilla in lobortis nec,
+            blandit non leo.
+          </p>
+        </section>
+        <section>
+          <h2>Section 2</h2>
+          <p>
+            Phasellus pharetra dolor ut dolor porta tristique. Sed rhoncus
+            sagittis nisi, sed cursus diam. Aliquam orci elit, fringilla in
+            lobortis nec, blandit non leo. Phasellus pharetra dolor ut dolor
+            porta tristique. Sed rhoncus sagittis nisi, sed cursus diam. Aliquam
+            orci elit, fringilla in lobortis nec, blandit non leo. Phasellus
+            pharetra dolor ut dolor porta tristique. Sed rhoncus sagittis nisi,
+            sed cursus diam. Aliquam orci elit, fringilla in lobortis nec,
+            blandit non leo.
+          </p>
+        </section>
+        <section>
+          <h2>Section 3</h2>
+          <p>
+            {" "}
+            Donec vitae lobortis urna. Nunc eu rutrum risus. Duis iaculis
+            efficitur efficitur. Praesent hendrerit, lorem at fermentum iaculis,
+            lorem libero placerat arcu, vel dignissim eros justo in arcu. Proin
+            felis metus, cursus non felis a, venenatis gravida turpis.
+            Suspendisse in elit eget quam sagittis venenatis. Morbi blandit quam
+            vel quam eleifend, non elementum velit venenatis. Nulla facilisi.
+            Nulla et libero congue, tincidunt tellus eu, laoreet neque.
+            Suspendisse elementum massa lacus, ac posuere sem fermentum semper.
+            Phasellus pharetra dolor ut dolor porta tristique. Sed rhoncus
+            sagittis nisi, sed cursus diam. Aliquam orci elit, fringilla in
+            lobortis nec, blandit non leo.
+          </p>
+        </section>
+        <section>
+          <h2>Section 4</h2>
+          <p>
+            Nulla bibendum ornare bibendum. Nam eget mauris fringilla, pretium
+            massa elementum, rutrum nunc. Donec sit amet laoreet lectus.
+            Suspendisse efficitur ex in eros porttitor, ac imperdiet lorem
+            interdum. Integer vehicula turpis ipsum, vitae iaculis magna
+            ultrices in. Sed pulvinar sodales mi, mollis molestie orci
+            scelerisque eget. Nulla bibendum ornare bibendum. Nam eget mauris
+            fringilla, pretium massa elementum, rutrum nunc. Donec sit amet
+            laoreet lectus. Suspendisse efficitur ex in eros porttitor, ac
+            imperdiet lorem interdum. Integer vehicula turpis ipsum, vitae
+            iaculis magna ultrices in. Sed pulvinar sodales mi, mollis molestie
+            orci scelerisque eget.
+          </p>
+        </section>
+      </div>
     </article>
-      
-    
-  )
+  );
 }
 
 function Footer() {
   return (
-    <div>
-      <footer className='page-footer'>
-        <h3>I am the footer</h3>
-      </footer>
-    </div>
-  )
+    <footer className="page-footer">
+      <h3>Copyright 2023</h3>
+    </footer>
+  );
 }
-
